768788733b6460f5d6e27f3b3e48c1041990a3f3
kilroy.tseng <kilroy.tseng@horizon.ai>
2019-01-30 14:19:21 +0800

    remote auto gen files

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 4 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#4 SMP PREEMPT Wed Jan 30 11:38:18 CST 2019"
-#define LINUX_COMPILE_BY "kilroy"
-#define LINUX_COMPILE_HOST "kilroy-VirtualBox"
-#define LINUX_COMPILER "gcc version 6.4.1 20170707 (Linaro GCC 6.4-2017.08)"
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "4.14.74-ge523f6f94-dirty"
